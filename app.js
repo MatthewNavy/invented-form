f370bfe186ca2532d466f45bcaa4da3199a4d27e
Matthew Barton <mbarton@trinity.edu>
2022-04-17 05:19:52 -0500

    Add init content and style

--- app.js
+++ app.js
@@ -2,12 +2,16 @@
 
 var express = require('express');
 var cors = require('cors');
-// var path = require('path');
+var path = require('path');
 
 var app = express();
 app.use(cors());
 
 const port = process.env.PORT || 3000;
+
+app.get('/js/poems.js', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/js/poems.js'));
+})
 
 app.use(express.static(__dirname + "/public/"));
 
---
+++ public/js/poems.js
@@ -0,0 +1,7 @@
+'use strict';
+
+
+function generate() {
+  const output = document.getElementById("output");
+  output.innerText = "output";
+}
